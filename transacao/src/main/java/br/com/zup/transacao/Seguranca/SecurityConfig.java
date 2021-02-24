b0015b6366103b2205582e6acac986f1e28e8695
eneri-jr <eneri.jr@gmail.com>
2021-02-24 09:39:27 -0300

    Seguranca na api

---
+++ transacao/src/main/java/br/com/zup/transacao/Seguranca/SecurityConfig.java
@@ -0,0 +1,17 @@
+package br.com.zup.transacao.Seguranca;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+                .anyRequest().authenticated()).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
